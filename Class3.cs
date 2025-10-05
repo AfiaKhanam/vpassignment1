e092fbb0257830b605e3fe6562ffd41c9fec2261
Afia Khanam <aafiakhanam2003@gmail.com>
2025-10-05 04:34:38 -0700

    Add files via upload

---
+++ Class3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Class3
+{
+	public Class3()
+	{
+        Console.Write("Enter marks (0 - 100): ");
+        int marks = Convert.ToInt32(Console.ReadLine());
+
+        if (marks >= 90 && marks <= 100)
+        {
+            Console.WriteLine("Grade: A");
+        }
+        else if (marks >= 80 && marks <= 89)
+        {
+            Console.WriteLine("Grade: B");
+        }
+        else if (marks >= 65 && marks <= 69)
+        {
+            Console.WriteLine("Grade: C");
+        }
+        else if (marks >= 50 && marks <= 60)
+        {
+            Console.WriteLine("Grade: D");
+        }
+        else if (marks >= 0 && marks < 50)
+        {
+            Console.WriteLine("Grade: F");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input! Please enter marks between 0 and 100.");
+        }
+    }
+}
+
