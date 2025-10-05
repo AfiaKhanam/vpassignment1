9fd96f5bf3e525a3b0644437748367896da26086
Afia Khanam <aafiakhanam2003@gmail.com>
2025-10-05 04:55:21 -0700

    Update Program.cs

--- vpassignment1/Program.cs
+++ vpassignment1/Program.cs
@@ -10,6 +10,16 @@ namespace vpassignment1
     {
         static void Main(string[] args)
         {
+                Class1.afia();
+                Class2.afia();
+                Class3.afia();
+                Class4.afia();
+                Class5.afia();
+                Class6.afia();
+                Class7.afia();
+                Class8.afia();
+                Class9.afia();
+                Class10.afia();
         }
     }
 }
